d06e3ac5a86c314c8a1e3100a9ddf46623571b7d
Leslie Dees <leslie.dees@duke.edu>
2024-02-21 18:45:31 -0500

    add gitignore, swap to llm api

--- test_mistral.py
+++ test_mistral.py
@@ -1,12 +1,28 @@
-from transformers import AutoTokenizer, AutoModelForCausalLM
-import torch
+# from transformers import AutoTokenizer, AutoModelForCausalLM
+# import torch
+# from accelerate import Accelerator
+# from accelerate.utils import gather_object
 
-# tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-7B-Instruct-v0.1")
-# model = AutoModelForCausalLM.from_pretrained("mistralai/Mistral-7B-Instruct-v0.1")
+# # tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-7B-Instruct-v0.1")
+# # model = AutoModelForCausalLM.from_pretrained("mistralai/Mistral-7B-Instruct-v0.1")
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-if torch.cuda.device_count() > 1:
-    print("Let's use", torch.cuda.device_count(), "GPUs!")
+# device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+# if torch.cuda.device_count() > 1:
+#     print(torch.cuda.device_count(), "GPUs")
     
 
-# model.to(device)
+# # model.to(device)
+
+
+from openai import OpenAI
+client = OpenAI()
+
+response = client.chat.completions.create(
+  model="gpt-3.5-turbo",
+  messages=[
+    {"role": "system", "content": "You are a helpful assistant."},
+    {"role": "user", "content": "Who won the world series in 2020?"},
+    {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
+    {"role": "user", "content": "Where was it played?"}
+  ]
+)
